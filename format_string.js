6105b645dfd29e342ebe799b5573645886a41b8c
Jesus Isaias Arevalo <isa90201@gmail.com>
2017-10-09 21:33:06 -0700

    Included a new function that takes ANY format_str and str_array and replaces {?} with items in str_array in order.

--- format_string.js
+++ format_string.js
@@ -1,3 +1,23 @@
+function getFormattedString2(format_str, str_array)
+{
+  let placeholder = '{?}';
+  let result = '';
+  
+  for (let i = 0; i < str_array.length; ++i) 
+  {
+    let curr_str = str_array[i]; 
+    if (result == '')
+    {
+      result = format_str.replace(placeholder, curr_str);
+    }
+    else
+    {
+      result = result.replace(placeholder, curr_str);
+    }
+  }
+  return result;
+}
+
 function getFormattedString(format_str, args) 
 {
   //console.log('\nFORMAT_STR = ' + format_str + ', ARGS = ' + args);
