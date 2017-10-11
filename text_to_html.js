94f8d7f9a663118f1032bfc9599f56d76bea6ec6
Jesus Isaias Arevalo <isa90201@gmail.com>
2017-10-10 19:13:53 -0700

    Fixed logic in toHtml( ). (It was throwing exceptions)

--- text_to_html.js
+++ text_to_html.js
@@ -45,34 +45,103 @@ class HtmlText
   }
 }
 
-function toHtml(textline, size, color)
+function toHtml(textline, size, color) // NOTE: textline trans_tokens should be transposed by this point.
 {
   if (!textline.needs_transposing)
   {
     console.log('toHtml():: NEEDS_TRANSPOSING=' + textline.needs_transposing);
 
     let html_text = new HtmlText(textline.format_str, size, 'normal', '#000000');
-    return html_text.string();
+    return html_text.html();
   }
   
   console.log('toHtml():: NEEDS_TRANSPOSING=Yes');
   let processed_tokens = textline.processed_tokens;
 
+  let curr_index = 0;
+  let pt_index = 0;
+
+  let start_end_index_sets = getStartAndEndIndexSets(textline.format_str);
+  let set_index = 0;
+
   let html = '';
-  for (let i = 0; i < processed_tokens.length; ++i)
+
+  while(curr_index < textline.format_str.length)
   {
-    let curr_token = processed_tokens[i];
-    if (curr_token.needs_transposing)
+    if (set_index < start_end_index_sets.length)
     {
-      let h = new HtmlText(curr_token.string, size, 'bold', '#000000'); // Bold (colored)
-      html += h.string();
+      let curr_set = start_end_index_sets[set_index]; // HERE NOW! FIXX !!!
+      set_index += 1; // REMOVE IF FAILS!
+
+      let h = null;
+      if (curr_set.start_index == 0 || curr_set.start_index == curr_index || curr_set.end_index == textline.format_str.length - 1)
+      {
+        // Append token string to HTML
+        let p_token = processed_tokens[pt_index];
+        pt_index += 1;
+        h = new HtmlText(p_token.string, size, 'bold', color); // Bold (colored)
+        html += h.string();
+      }
+      else
+      {
+        if (pt_index >= processed_tokens.length)
+        {
+          // Append plaintext to HTML
+          let f_substr = textline.format_str.substring(curr_index, textline.format_str.length - 1);
+          h = new HtmlText(f_substr, size, 'normal', '#000000'); // Normal (black)
+          html += h.string();
+          break;
+        }
+        else
+        {
+          // Append plaintext to HTML
+          let f_substr = textline.format_str.substring(curr_index, curr_set.start_index);
+          h = new HtmlText(f_substr, size, 'normal', '#000000'); // Normal (black)
+          html += h.string();
+
+          // Append token string to HTML
+          let p_token = processed_tokens[pt_index];
+          pt_index += 1;
+          h = new HtmlText(p_token.string, size, 'bold', color); // Bold (colored)
+          html += h.string();
+        }
+      }
+      curr_index = curr_set.end_index + 1;
+    }
+  }
+
+  console.log('  HTML --> ' + html);
+  return html;
+}
+
+function getStartAndEndIndexSets(format_str)
+{
+  let sets = [];
+
+  let placeholder = '{?}';
+  let start_index = format_str.indexOf(placeholder);
+  let substr = format_str;
+  let adjustment = format_str.length - substr.length;
+  let curr_index = 0;
+
+  console.log('  *** FORMAT_STR=' + format_str);
+  while (start_index >= 0)
+  {
+    let set = {'start_index': start_index + adjustment, 'end_index': start_index + (placeholder.length - 1) + adjustment};
+    console.log('INDEX_SET=' + JSON.stringify(set));
+    sets.push(set);
+
+    if (set.end_index < format_str.length - 1)
+    {
+      substr = format_str.substring(set.end_index + 1);
+      adjustment = format_str.length - substr.length;
+      console.log('  ** NEW_SUBSTR=' + substr);
+      start_index = substr.indexOf(placeholder);
     }
     else
     {
-      let h = new HtmlText(curr_token.string, size, 'normal', '#000000'); // Plaintext (black)
-      html += h.string();
+      start_index = -1;
     }
   }
-  console.log('  HTML --> ' + html);
-  return html;
+  return sets;
 }
