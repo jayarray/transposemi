ea563df0e59d576ad5e16226dd376f74ee89aa86
Jesus Isaias Arevalo <isa90201@gmail.com>
2017-10-10 22:23:14 -0700

    Replaced old method with new method and renamed second method like the first.

--- format_string.js
+++ format_string.js
@@ -1,4 +1,4 @@
-function getFormattedString2(format_str, str_array)
+function getFormattedString(format_str, str_array)
 {
   let placeholder = '{?}';
   let result = '';
@@ -13,36 +13,6 @@ function getFormattedString2(format_str, str_array)
     else
     {
       result = result.replace(placeholder, curr_str);
-    }
-  }
-  return result;
-}
-
-function getFormattedString(format_str, args) 
-{
-  //console.log('\nFORMAT_STR = ' + format_str + ', ARGS = ' + args);
-
-  let result = '';
-
-  for (let i = 0; i < args.length; ++i) 
-  {
-    let curr_arg = args[i];
-    let placeholder_str = '{' + i + '}';
-    //console.log('  i = ' + i +', CURR_PLACEHOLDER: ' + placeholder_str); // DEBUG
-
-    if (result == '')
-    {
-      result = format_str.replace(placeholder_str, curr_arg);
-    }
-    else
-    {
-      result = result.replace(placeholder_str, curr_arg);
-    }
-
-    //console.log('  CURR_RESULT = ' + result); // DEBUG
-    while (result.includes(placeholder_str))
-    {
-      result = result.replace(placeholder_str, curr_arg);
     }
   }
   return result;
